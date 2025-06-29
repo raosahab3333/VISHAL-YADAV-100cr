a13e6eac8f627f3a5ff5ed5c39940859d909c4c6
raosahab3333 <vy807924@gmail.com>
2025-06-29 09:07:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,4 +9,6 @@ def home():
     return render_template("index.html", stocks=results)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
